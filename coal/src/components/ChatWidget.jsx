b8838192ed04638143a91e7f18017aedfb010ab4
Ankit-1204 <ankit2003upadhyay@gmail.com>
2025-03-07 04:22:25 +0530

    adding tailwind, improve Chatbot Widget

--- coal/src/components/ChatWidget.jsx
+++ coal/src/components/ChatWidget.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import { Chat } from './ChatWindow'
 import  {FontAwesomeIcon}  from '@fortawesome/react-fontawesome'
 import { faMessage, faPenNib } from '@fortawesome/free-solid-svg-icons'
-export const ChatWidget= ()=> {
+export const ChatWidget= ({apiUrl, initialPrompt, widgetProp ,windowProp , messageProp})=> {
     const [active,setActive]=useState(false)
     function handleClick (){
         setActive(!active)
     }
     return(
-        <div className='body'>
+        <div className={'widgetProp'}>
             {active ? <Chat/> :<button onClick={()=>handleClick()}> <FontAwesomeIcon icon={faMessage} /> </button>}
         </div>
 
--- coal/src/components/ChatWindow.jsx
+++ coal/src/components/ChatWindow.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 import './Window.css'
-export const Chat= ()=>{
+export const Chat= ({apiUrl, initialPrompt,windowProp , messageProp})=>{
+    const [input, setInput] = useState("");
     return(
-        <div className='chatWindow'>hello</div>
+        <div className={'flex flex-col w-full overflow-hidden pb-[3px] bg-cyan-500 size-64 windowProp'}>
+           <div className= {'flex py-[5px] bg-cyan-300 justify-center messageProp'}>ChatBot</div> 
+           <div className='flex-grow'></div>
+           <div className="flex mx-[1px] px-[2px]">
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type a message..." />
+            <button >Send</button>
+            </div>
+        </div>
+        
     )
 }
