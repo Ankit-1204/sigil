e9766ae9bd687ef74a83bb978d9faacbb6fe56f1
Ankit-1204 <ankit2003upadhyay@gmail.com>
2025-03-09 01:00:45 +0530

    Implement api backend. Openai no free tier, shift to gemini

--- coal/src/components/ChatWindow.jsx
+++ coal/src/components/ChatWindow.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
+import axios from "axios"
 import './Window.css'
+const API = 'http://127.0.0.1:8080/chat'
 export const Chat= ({apiUrl, initialPrompt,windowProp , messageProp})=>{
     const [input, setInput] = useState("");
+    initialPrompt="You are a helpfull bot"
+    const [messages, setMessages] = useState([]);
+    useEffect( () =>{
+        async () =>{
+           if(initialPrompt){
+            await axios.post(API,{message: messages})
+            } 
+        } 
+    },[initialPrompt])
+    const handleSend= async ()=>{
+        if(!input.trim()){
+            return;
+        }
+        const newMessage = [...messages,{ text: input, sender: "user" }];
+        setMessages(newMessage)
+        setInput('')
+
+        const response= await axios.post(API,newMessage)
+        console.log(response.data.item.message)
+        const ind=response.data.item.message.length
+        setMessages([...newMessage,{text:response.data.item.message[ind-1]['text'], sender:'bot'}])
+    }
     return(
         <div className={'flex flex-col w-full overflow-hidden pb-[3px] bg-cyan-500 size-64 windowProp'}>
            <div className= {'flex py-[5px] bg-cyan-300 justify-center messageProp'}>ChatBot</div> 
-           <div className='flex-grow'></div>
+           <div className='flex-grow'>
+            {messages.map((msg,index)=>(<h3 key={index}>{msg.text}, {msg.sender}</h3>))}
+           </div>
            <div className="flex mx-[1px] px-[2px]">
             <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type a message..." />
-            <button >Send</button>
+            <button onClick={handleSend}>Send</button>
             </div>
         </div>
         
---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express= require('express')
+const cors = require('cors')
+require("dotenv").config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const app=express()
+const PORT = process.env.PORT || 5000;
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API)
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+
+app.post('/chat', async (req,res)=>{
+    try {
+        const message = req.body;
+        console.log(message)
+        const resp = await model.generateContent(message,);
+        console.log(resp.response.text())
+        res.json({ reply: resp.response.text() });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: error.message });
+    }
+})
+app.listen(PORT,()=>{
+    console.log(`Server running on ${PORT}`)
+})
